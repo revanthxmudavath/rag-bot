8a855b383c25d617024530ff197821afab3df24e
Skateslavasker <revanthnaik12@gmail.com>
2025-09-30 13:46:00 -0700

    "feature/rag-backend: Updated repository"

--- test1.py
+++
@@ -1,6 +0,0 @@
-from pymongo.mongo_client import MongoClient
-from pymongo.server_api import ServerApi
-
-uri = "mongodb-uri-here"
-client = MongoClient(uri, server_api=ServerApi('1'))
-print(client.admin.command("ping"))
